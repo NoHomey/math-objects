59bc231214ffae4a42393bc0fbd888183b971f61
Ivo Stratev <ivo.stratev.tues@gmail.com>
2016-11-22 22:49:23 +0200

    Update examples.cc

--- examples.cc
+++ examples.cc
@@ -2,13 +2,22 @@
 
 #include <iostream>
 
-int main() {
-    math_objects::linear_algebra::Matrix<2, 2, int> m = {
-        1, 2,
-        3, 4   
-    };
+using math_objects::linear_algebra::Matrix;
 
-    std::cout << m(2, 2) << std::endl;
+int main() {
+    Matrix<2, 3, int> m = {
+        1, 3, 5,
+        2, 4, 6   
+    };
+    auto mt = m.T();
+    std::cout << (m + m) << std::endl;
+    std::cout << (9 * m) << std::endl;
+    std::cout << m << std::endl;
+    std::cout << mt << std::endl;
+    std::cout << (m * mt) << std::endl;
+    std::cout << m(2, 3) << std::endl;
+    std::cout << mt(3, 2) << std::endl;
+    std::cout << (Matrix<2, 2, int>({0, 1, 0, 0}) * Matrix<2, 2, int>({0, 0, 0, 1})) << std::endl;
 
     return 0;
 }
